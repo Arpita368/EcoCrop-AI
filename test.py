5f535160a65d545eb45d5c0ff727d094bfd666a6
Arpita Sonparote <arpita.j.s.368@gmail.com>
2025-11-02 01:17:03 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyCqxahdXW63NEM4rtkDQ-EhlYHLa3AKKOU")
-
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
-response = model.generate_content("Hello Gemini!")
-print(response.text)
